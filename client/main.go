6894dba8f53d9746a7bc0a789ca5982061702636
fadasd <irfanoezkan.65@gmail.com>
2026-04-26 17:54:37 +0200

    done

--- client/main.go
+++ client/main.go
@@ -18,4 +18,24 @@ func main() {
 	body, _ := io.ReadAll(resp.Body)
 
 	fmt.Println(string(body))
+
+	url = "http://api:8080/books/create?title=NewBook&author_id=1&price=100&stock=25"
+
+	resp, err = http.Get(url)
+	if err != nil {
+		panic(err)
+	}
+	body, _ = io.ReadAll(resp.Body)
+
+	fmt.Println(string(body))
+
+	url = "http://api:8080/books/get?page=1&limit=10&in_stock=true"
+
+	resp, err = http.Get(url)
+	if err != nil {
+		panic(err)
+	}
+	body, _ = io.ReadAll(resp.Body)
+
+	fmt.Println(string(body))
 }
--- simpleHTTPServer/main.go
+++ simpleHTTPServer/main.go
@@ -30,6 +30,7 @@ func main() {
 
 	http.HandleFunc("/books/id/", getBookByIDHandler)
 	http.HandleFunc("/books/create", createBookHandler)
+	http.HandleFunc("/books/get", getBooksHandler)
 
 	log.Println("API running on :8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
@@ -89,4 +90,83 @@ func createBookHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	fmt.Fprintln(w, "book created")
+}
+
+func getBooksHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain")
+
+	pageStr := r.URL.Query().Get("page")
+	limitStr := r.URL.Query().Get("limit")
+	authorID := r.URL.Query().Get("author_id")
+	minPrice := r.URL.Query().Get("min_price")
+	maxPrice := r.URL.Query().Get("max_price")
+	inStock := r.URL.Query().Get("in_stock")
+
+	page := 1
+	limit := 10
+
+	if pageStr != "" {
+		fmt.Sscanf(pageStr, "%d", &page)
+	}
+	if limitStr != "" {
+		fmt.Sscanf(limitStr, "%d", &limit)
+	}
+
+	offset := (page - 1) * limit
+
+	query := `
+		SELECT id, title, author_id, price, stock
+		FROM books
+		WHERE 1=1
+	`
+
+	args := make([]any, 0)
+	argID := 1
+
+	if authorID != "" {
+		query += fmt.Sprintf(" AND author_id = $%d", argID)
+		args = append(args, authorID)
+		argID++
+	}
+
+	if minPrice != "" {
+		query += fmt.Sprintf(" AND price >= $%d", argID)
+		args = append(args, minPrice)
+		argID++
+	}
+
+	if maxPrice != "" {
+		query += fmt.Sprintf(" AND price <= $%d", argID)
+		args = append(args, maxPrice)
+		argID++
+	}
+
+	if inStock == "true" {
+		query += " AND stock > 0"
+	}
+
+	query += fmt.Sprintf(" LIMIT $%d OFFSET $%d", argID, argID+1)
+	args = append(args, limit, offset)
+
+	rows, err := db.Query(query, args...)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var id, authorID, stock int
+		var title string
+		var price float64
+
+		err := rows.Scan(&id, &title, &authorID, &price, &stock)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		fmt.Fprintf(w, "ID:%d | %s | Author:%d | %.2f | Stock:%d\n",
+			id, title, authorID, price, stock)
+	}
 }
